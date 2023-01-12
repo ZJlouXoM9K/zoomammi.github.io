d0896dc96daf3d299428b4e6125c556bd27a8aee
Джалал <42112271+ZJlouXoM9K@users.noreply.github.com>
2023-01-13 00:21:01 +0400

    Add files via upload

---
+++ ZooMammy/script/script.js
@@ -0,0 +1,36 @@
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}
+
+	var accordion = new Accordion($('#accordion'), false);
+});
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items:1,
+		dots: false,
+    loop:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+  });
+});
